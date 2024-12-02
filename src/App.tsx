e53592da0977b64f639a2f296b923a763355b35d
cristian <cristiansala894@gmail.com>
2024-12-02 16:05:54 +0100

    pagina details bianca

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 // src/App.tsx
+import { Link } from 'react-router';
 import './App.css';
 import { CardContainer } from './components/CardContainer';
+import { Text } from "@chakra-ui/react";
 
 function App() {
   return (
     <div>
       <h1>MovieApp</h1>
+      <Text mt={2}>
+        <Link to="/details">Vai ai dettagli</Link>
+      </Text>
       {/* Componenti CardContainer per i diversi tipi di contenuto */}
       <CardContainer url="trending/movie/day" section="Trending Movies" />
-      <CardContainer url="trending/person/day" section="Trending People"/>
+      <CardContainer url="trending/person/day" section="Trending People" />
       <CardContainer url="trending/tv/day" section="Trending TV" />
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
-import { Provider } from "./components/ui/provider.tsx"
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { Provider } from "./components/ui/provider.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Details } from "./components/Details";
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <BrowserRouter basename="/movie-app-react">
+        <Routes>
+          <Route index element={<App />} />
+          <Route path="/details" element={<Details />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 )
