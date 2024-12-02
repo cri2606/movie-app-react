bda08d3b65970b77d835c27b2ae6d7ada10c0ad7
cristian <cristiansala894@gmail.com>
2024-12-02 12:01:13 +0100

    tolto undefined nella funzione srcImage

--- src/functions/functions.tsx
+++ src/functions/functions.tsx
@@ -1,6 +1,6 @@
 import { CardType } from "../types/cardType";
 // Funzione per visualizzare l'immagine, passo il percoso dell'immagine
-export const srcImage = (element: CardType, path: string | undefined) => {
+export const srcImage = (element: CardType, path: string) => {
     if (path) 
         return `https://image.tmdb.org/t/p/w500${path}`;
     return element.media_type === 'person' ? '/movie-app-react/images/person_fallback.png' : '/movie-app-react/images/movie_tv_fallback.png';
